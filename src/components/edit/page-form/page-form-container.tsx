7f5bec3342ffa5eeed336bcdb256a875f853d456
Thomas Bakker <thomasbakker@hotmail.co.uk>
2024-12-30 12:46:38 +0000

    Reset page form on route chage

--- src/components/edit/page-form/page-form-container.tsx
+++ src/components/edit/page-form/page-form-container.tsx
@@ -4,6 +4,7 @@ import { FieldValues, FormProvider, useForm } from "react-hook-form";
 import { PageForm } from "./page-form";
 import { PageFormContextProvider } from "./context/page-form-context";
 import { IPage } from "@/types/page";
+import { useEffect } from "react";
 
 export const PageFormContainer = ({ pageData }: { pageData: IPage }) => {
 	const methods = useForm({
@@ -12,6 +13,10 @@ export const PageFormContainer = ({ pageData }: { pageData: IPage }) => {
 		shouldUnregister: true,
 		defaultValues: pageData as FieldValues,
 	});
+
+	useEffect(() => {
+		methods.reset(pageData);
+	}, [pageData]);
 
 	return (
 		<FormProvider {...methods}>
