68313a7d5788e63a5c86028137aeecc26e4f97b7
Thomas Bakker <thomasbakker@hotmail.co.uk>
2024-12-31 14:18:46 +0000

    edit fixes clean up

---
+++ src/components/edit/navigation/edit-navigation.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import styles from "./edit-navigation.module.scss";
+import { NavigationMenu } from "@/components/header/navigation/navigation-menu";
+import { getMainHeader, getSubHeaders } from "@/actions/header/get-header";
+import { SubHeaders } from "@/components/header/sub-headers";
+
+type EditNavigationProps = {
+	route: string;
+	isAdminEdit?: boolean;
+};
+
+export const EditNavigation = async ({
+	route,
+	isAdminEdit,
+}: EditNavigationProps) => {
+	const mainHeader = await getMainHeader();
+	const subHeaders = await getSubHeaders(route);
+
+	// Get this from somewhere
+	const homeHref = isAdminEdit ? "/admin" : `/edit`;
+	const homeLabel = isAdminEdit ? "Home" : `User Home`;
+
+	const routePrefix = isAdminEdit ? "/admin?route=" : "/edit?route=";
+	return (
+		<section className={styles.header}>
+			<Link href={homeHref}>{homeLabel}</Link>
+			{/* BACK BUTTON / More up a level button */}
+			{isAdminEdit ? (
+				<NavigationMenu items={mainHeader.nav} prefix={routePrefix} />
+			) : null}
+			{subHeaders && subHeaders.length > 0 ? (
+				<SubHeaders headersArray={subHeaders} prefix={routePrefix} />
+			) : null}
+		</section>
+	);
+};
