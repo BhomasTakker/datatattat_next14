20d90d58bd7435a1065e489d74698c3bb2912df4
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-01-21 19:05:48 +0000

    dirty prod fix
    removing caching for now - limit got battered

--- src/lib/redis/db.ts
+++ src/lib/redis/db.ts
@@ -6,18 +6,20 @@ import { REDIS_URL } from "./config";
 let cachedConnection: Redis | null = null;
 
 export const connectToRedisDB = () => {
+	// console.log("Connecting to redis db");
+	throw new Error("Not implemented properly");
 	if (cachedConnection) {
 		// console.log("Using cached redis db connection");
 		return cachedConnection;
 	}
 	try {
-		const cnx = new Redis(REDIS_URL);
+		const cnx = new Redis(REDIS_URL, { showFriendlyErrorStack: true });
 		cachedConnection = cnx;
 		console.log("New redis db connection established");
 		return cachedConnection;
 	} catch (error) {
 		// If an error occurs during connection, log the error and throw it
-		console.log(error);
+		// console.log(error);
 		throw error;
 	}
 };
--- src/lib/redis/redis-fetch.ts
+++ src/lib/redis/redis-fetch.ts
@@ -1,3 +1,5 @@
+// @ts-ignore - Ignoring until we have a better solution for redis
+
 import { UnknownObject } from "@/types/utils";
 import { connectToRedisDB } from "./db";
 
@@ -9,17 +11,23 @@ export const fetchWithCache = async (
 	key: string,
 	expiry: number = 60 * 60 * 24
 ) => {
-	const client = connectToRedisDB();
-	const cachedData = await client.get(key);
-	if (cachedData) {
-		return JSON.parse(cachedData);
+	try {
+		const client = connectToRedisDB();
+		const cachedData = await client.get(key);
+		if (cachedData) {
+			return JSON.parse(cachedData);
+		}
+		const data = await fetchFn();
+		// set one day cache for now :/
+		// redis upstash is better at this?
+		await client.set(key, JSON.stringify(data), "EX", expiry);
+		// don't overwrite a lower cache
+		// is for redis upstash
+		// await client.expire(key.toString(), cacheExpire, "lt");
+		return data;
+	} catch (error) {
+		// No more cache!
+		const data = await fetchFn();
+		return data;
 	}
-	const data = await fetchFn();
-	// set one day cache for now :/
-	// redis upstash is better at this?
-	await client.set(key, JSON.stringify(data), "EX", expiry);
-	// don't overwrite a lower cache
-	// is for redis upstash
-	// await client.expire(key.toString(), cacheExpire, "lt");
-	return data;
 };
