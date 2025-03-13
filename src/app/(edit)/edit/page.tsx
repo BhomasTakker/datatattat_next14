b0b63e666b4705d9f76e8f520dd6862370afbe9b
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-03-13 14:20:01 +0000

    try fix edit mongoose connection error

--- src/app/(edit)/edit/page.tsx
+++ src/app/(edit)/edit/page.tsx
@@ -3,10 +3,12 @@ import { PATHS } from "@/lib/routing/paths";
 import isSignupComplete from "@/actions/signup/signup-completed";
 import isValidSession from "@/actions/auth/check-session";
 import { isValidUser } from "@/actions/auth/check-valid-user";
+import { connectToMongoDB } from "@/lib/mongo/db";
 
 // export const dynamic = "force-dynamic";
 
 export default async function Page() {
+	await connectToMongoDB();
 	await isValidSession();
 	await isSignupComplete();
 	const { username } = await isValidUser();
--- src/app/(edit)/edit/users/page.tsx
+++ src/app/(edit)/edit/users/page.tsx
@@ -1,10 +1,12 @@
 import isValidSession from "@/actions/auth/check-session";
 import { isValidUser } from "@/actions/auth/check-valid-user";
 import isSignupComplete from "@/actions/signup/signup-completed";
+import { connectToMongoDB } from "@/lib/mongo/db";
 import { PATHS } from "@/lib/routing/paths";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
+	await connectToMongoDB();
 	await isValidSession();
 	await isSignupComplete();
 	const { username } = await isValidUser();
