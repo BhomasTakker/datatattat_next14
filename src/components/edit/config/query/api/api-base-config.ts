ee8455d1fdc79d307a9ef68355137fe06bd0c7b9
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-03-22 11:56:59 +0000

    integrate youtube-api

--- src/components/edit/config/query/api/api-base-config.ts
+++ src/components/edit/config/query/api/api-base-config.ts
@@ -1,10 +1,13 @@
 import { EditInputs } from "@/components/edit/inputs/inputs";
 import { InputListProps } from "@/types/edit/inputs/inputs";
 import { ARTICLES_SEARCH_API_CONFIG } from "./apis/articles-seacrh-api";
+import { YOUTUBE_API_CONFIG } from "./apis/youtube-api";
 
+// Should be more global type/ struct we use these options elsewhere
 const APIOptions = {
 	NONE: "none",
 	ARTICLES_SEARCH_API: "articles-search-api",
+	YOUTUBE_API: "youtube-api",
 } as const;
 
 type apiContainersProps = null | typeof ARTICLES_SEARCH_API_CONFIG;
@@ -12,6 +15,7 @@ type apiContainersProps = null | typeof ARTICLES_SEARCH_API_CONFIG;
 const apiMap = new Map<string, apiContainersProps>([
 	[APIOptions.NONE, null],
 	[APIOptions.ARTICLES_SEARCH_API, ARTICLES_SEARCH_API_CONFIG],
+	[APIOptions.YOUTUBE_API, YOUTUBE_API_CONFIG],
 ]);
 
 export const API_BASE_QUERY_CONFIG: InputListProps = {
--- src/lib/api/component-data/api/api-fetch.ts
+++ src/lib/api/component-data/api/api-fetch.ts
@@ -1,13 +1,20 @@
 import { searchArticles } from "@/lib/mongo/actions/articles/search";
 import { WithQuery } from "@/types/page";
 import { cloneDeep } from "@/utils/object";
+import { youtubeApiFetch } from "../google/youtube/youtube-api";
 
-const apiMap = new Map([
+type APIOptions = typeof searchArticles | typeof youtubeApiFetch | null;
+
+const apiMap = new Map<string, APIOptions>([
 	["none", null],
 	["articles-search-api", searchArticles],
+	["youtube-api", youtubeApiFetch],
 ]);
 
+// We need to specify a cache time for the data
 export const apiFetch = async (query: WithQuery) => {
+	// query should have a second passed object as standard
+	// containing cache time etc - & we should have a default
 	const { params, provider } = query;
 
 	const api = apiMap.get(provider);
