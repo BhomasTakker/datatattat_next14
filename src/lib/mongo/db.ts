69a49b9c2bd26daf712bc88a477bbb0696d3a444
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-07 19:41:57 +0100

    remove index creation
    clean up file

--- src/lib/mongo/db.ts
+++ src/lib/mongo/db.ts
@@ -2,37 +2,24 @@
 // Importing mongoose library along with Connection type from it
 import mongoose, { Connection } from "mongoose";
 
-// Declaring a variable to store the cached database connection
+// We can set indexes on atlas / we should also create a search instead of an index
+// https://www.youtube.com/playlist?list=PL4RCxklHWZ9sEaTrqx7DOxQ5FzwDOTWFj
+
 let cachedConnection: Connection | null = null;
 
-// Function to establish a connection to MongoDB
 export async function connectToMongoDB() {
-	// If a cached connection exists, return it
 	if (cachedConnection) {
 		console.log("Using cached db connection");
 		return cachedConnection;
 	}
 	try {
-		// Check the thing exists and throw an error if it doesn'ts
-		// If no cached connection exists, establish a new connection to MongoDB
 		const cnx = await mongoose.connect(process.env.MONGODB_URI!);
-		// Cache the connection for future use
 		cachedConnection = cnx.connection;
-		// Log message indicating a new MongoDB connection is established
-		console.log("New mongodb connection established");
-		// Return the newly established connection
 
-		// not here / we can create a search index on Atlas
-		// view and properly investigate
-		// https://www.youtube.com/playlist?list=PL4RCxklHWZ9sEaTrqx7DOxQ5FzwDOTWFj
-		const articlesCollection = cachedConnection.collection("articles");
-		await articlesCollection.createIndexes([
-			{ name: "text", key: { title: "text" } },
-		]);
+		console.log("New mongodb connection established");
 
 		return cachedConnection;
 	} catch (error) {
-		// If an error occurs during connection, log the error and throw it
 		console.log(error);
 		throw error;
 	}
