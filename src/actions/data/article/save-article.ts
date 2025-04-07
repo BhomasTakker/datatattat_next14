64e2d5917bf8c597481d3ce0cd1009d03ff38c56
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-08 00:49:35 +0100

    missing db loads

--- src/actions/data/article/save-article.ts
+++ src/actions/data/article/save-article.ts
@@ -4,6 +4,7 @@ import { saveOrCreateArticleBySrc } from "@/lib/mongo/actions/article";
 import { CollectionItem } from "@/types/data-structures/collection/item/item";
 import { UnknownObject } from "@/types/utils";
 import { validateArticleData } from "./utils";
+import { initialiseServices } from "@/lib/services/intialise-services";
 
 // Stop gap - we need to properly define all of our Article Types
 type SaveArticleItem = CollectionItem & UnknownObject;
@@ -11,6 +12,7 @@ type SaveArticleItem = CollectionItem & UnknownObject;
 export const saveOrUpdateArticle = async (item: SaveArticleItem) => {
 	// validate item
 	if (validateArticleData(item)) {
+		await initialiseServices();
 		return await saveOrCreateArticleBySrc(item);
 	}
 	return Promise.resolve(null);
