af4bb33196a30352d74426d153d7b1059e9b15b4
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-08 00:49:04 +0100

    update connectToDB function and add ref

--- src/lib/mongo/db.ts
+++ src/lib/mongo/db.ts
@@ -7,7 +7,7 @@ import mongoose, { Connection } from "mongoose";
 
 let cachedConnection: Connection | null = null;
 
-export async function connectToMongoDB() {
+export async function connectToMongoDB_Old() {
 	if (cachedConnection) {
 		console.log("Using cached db connection");
 		return cachedConnection;
@@ -23,4 +23,45 @@ export async function connectToMongoDB() {
 		console.log(error);
 		throw error;
 	}
+}
+
+// https://github.com/vercel/next.js/blob/canary/examples/with-mongodb-mongoose/lib/dbConnect.ts
+declare global {
+	var mongoose: any; // This must be a `var` and not a `let / const`
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToMongoDB() {
+	const MONGODB_URI = process.env.MONGODB_URI!;
+
+	if (!MONGODB_URI) {
+		throw new Error(
+			"Please define the MONGODB_URI environment variable inside .env.local"
+		);
+	}
+
+	if (cached.conn) {
+		return cached.conn;
+	}
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false,
+		};
+		cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+			return mongoose;
+		});
+	}
+	try {
+		cached.conn = await cached.promise;
+	} catch (e) {
+		cached.promise = null;
+		throw e;
+	}
+
+	return cached.conn;
 }
