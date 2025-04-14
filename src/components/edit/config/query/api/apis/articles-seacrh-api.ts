c0859772585e9e470c7127d6e152b716cea5f5c5
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-14 20:36:07 +0100

    convert variant to an object select

--- src/components/edit/config/query/api/apis/articles-seacrh-api.ts
+++ src/components/edit/config/query/api/apis/articles-seacrh-api.ts
@@ -7,49 +7,7 @@ import { GenericInput, InputListProps } from "@/types/edit/inputs/inputs";
 // VIDEO_PARAMS
 // AUDIO_PARAMS
 
-export const PARAMS: GenericInput[] = [
-	{
-		id: "query",
-		type: EditInputs.text,
-		label: "Text Search",
-		required: false,
-		// searches title / use - to exclude words 'to search a phrase'
-	},
-	// Only when query added
-	{
-		id: "textScore",
-		type: EditInputs.number,
-		label: "Text Score",
-		required: false,
-		step: 0.01,
-		min: 0,
-		// searches title / use - to exclude words 'to search a phrase'
-	},
-	////////////////////////////////////////////////
-	// We need to vary inputs based on the variant
-	////////////////////////////////////////////////
-	{
-		id: "variant",
-		type: EditInputs.select,
-		label: "Variant",
-		options: ["article", "video", "audio"],
-		defaultValue: "article",
-	},
-	// only for audio
-	{
-		id: "durationHigher",
-		type: EditInputs.number,
-		label: "Duration (seconds) Higher Than",
-		min: 0,
-		required: false,
-	},
-	{
-		id: "durationLower",
-		type: EditInputs.number,
-		label: "Duration (seconds) Lower Than",
-		min: 0,
-		required: false,
-	},
+const BASE_PARAMS: GenericInput[] = [
 	{
 		// Select from available options uk, us, world, all
 		// select? or array? / multi select
@@ -103,6 +61,84 @@ export const PARAMS: GenericInput[] = [
 		label: "Limit",
 		required: false,
 	},
+];
+
+export enum Variants {
+	article = "article",
+	video = "video",
+	audio = "audio",
+}
+
+const ARTICLE_PARAMS: InputListProps = {
+	id: "stackScroller",
+	type: EditInputs.inputList,
+	label: "Stack Scroller",
+	inputs: [],
+};
+
+const VIDEO_PARAMS: InputListProps = {
+	id: "stackScroller",
+	type: EditInputs.inputList,
+	label: "Stack Scroller",
+	inputs: [],
+};
+
+const AUDIO_PARAMS: InputListProps = {
+	id: "audio-params",
+	type: EditInputs.inputList,
+	inputs: [
+		{
+			id: "durationHigher",
+			type: EditInputs.number,
+			label: "Duration (seconds) Higher Than",
+			min: 0,
+			required: false,
+		},
+		{
+			id: "durationLower",
+			type: EditInputs.number,
+			label: "Duration (seconds) Lower Than",
+			min: 0,
+			required: false,
+		},
+	],
+};
+
+const variantMap = new Map<string, InputListProps>([
+	[Variants.article, ARTICLE_PARAMS],
+	[Variants.video, VIDEO_PARAMS],
+	[Variants.audio, AUDIO_PARAMS],
+]);
+
+export const PARAMS: GenericInput[] = [
+	{
+		id: "variant",
+		type: EditInputs.objectSelect,
+		label: "Variant",
+		defaultValue: "article",
+		required: true,
+		options: [Variants.article, Variants.video, Variants.audio],
+		optionMap: variantMap,
+		// we are saved on comopnent props object - our parent
+		optionId: undefined, // "variantProps",
+	},
+	{
+		id: "query",
+		type: EditInputs.text,
+		label: "Text Search",
+		required: false,
+		// searches title / use - to exclude words 'to search a phrase'
+	},
+	{
+		id: "textScore",
+		type: EditInputs.number,
+		label: "Text Score",
+		required: false,
+		step: 0.01,
+		min: 0,
+		// searches title / use - to exclude words 'to search a phrase'
+	},
+	...BASE_PARAMS,
 ];
 
 export const ARTICLES_SEARCH_API_CONFIG: InputListProps = {
