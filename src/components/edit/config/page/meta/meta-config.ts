3c02c293443d779db37b51c2195858c3f467521f
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-15 21:07:29 +0100

    Update to show/hide meta data

--- src/components/edit/config/page/meta/meta-config.ts
+++ src/components/edit/config/page/meta/meta-config.ts
@@ -1,67 +1,62 @@
-import { InputListProps } from "@/types/edit/inputs/inputs";
+import { InputListProps, ShowInputProps } from "@/types/edit/inputs/inputs";
 import { EditInputs } from "../../../inputs/inputs";
 import { CARD_DATA } from "./card-data-config";
 import { FAV_ICON } from "./fav-icon-config";
 
 export const META_CONFIG: InputListProps = {
+	// this isn't the id!!!!!!
 	id: "meta",
 	type: EditInputs.inputList,
 	label: "Meta Object",
 
 	inputs: [
 		{
-			id: "metaPropertiesTitle",
-			type: EditInputs.title,
-			title: "Meta Properties",
-		},
+			id: "createMetaData",
+			type: EditInputs.show,
+			label: "Create Page Meta Data",
+			defaultChecked: false,
+			inputs: [
+				{
+					id: "cardData",
+					type: EditInputs.inputList,
+					label: "Card Data",
+					createObject: false,
 
-		{
-			id: "pageTitle",
-			type: EditInputs.text,
-			label: "Page Title",
+					inputs: [
+						{
+							id: "metaPropertiesTitle",
+							type: EditInputs.title,
+							title: "Meta Properties",
+						},
+						{
+							id: "pageTitle",
+							type: EditInputs.text,
+							label: "Page Title",
+							required: false,
+						},
+						{
+							id: "pageDescription",
+							type: EditInputs.text,
+							label: "Page Description",
+							required: false,
+						},
+						{
+							id: "pageKeywords",
+							type: EditInputs.text,
+							label: "Page Keywords",
+							required: false,
+						},
+						{
+							id: "pageImage",
+							type: EditInputs.text,
+							label: "Page Image",
+							required: false,
+						},
+						FAV_ICON,
+						CARD_DATA,
+					],
+				},
+			],
 		},
-		{
-			id: "pageDescription",
-			type: EditInputs.text,
-			label: "Page Description",
-		},
-		{
-			id: "pageKeywords",
-			type: EditInputs.text,
-			label: "Page Keywords",
-		},
-		{
-			id: "pageImage",
-			type: EditInputs.text,
-			label: "Page Image",
-		},
-		FAV_ICON,
-		CARD_DATA,
-		// Should be created by the system
-		// {
-		// 	id: "pageType",
-		// 	type: EditInputs.text,
-		// 	label: "Page Type",
-		// },
-		// {
-		// 	id: "pageUrl",
-		// 	type: EditInputs.text,
-		// 	label: "Page URL",
-		// },
-		// {
-		// 	id: "pageAuthor",
-		// 	type: EditInputs.text,
-		// 	label: "Page Author",
-		// },
-		// {
-		// 	id: "pagePublished",
-		// 	type: EditInputs.text,
-		// 	label: "Page Published",
-		// },
-		// {
-		// 	id: "pageModified",
-		// 	type: EditInputs.text,
-		// 	label: "Page Modified",
-		// },
 	],
 };
