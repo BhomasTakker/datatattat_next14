702ef604778d0e292a4adc95a29c52b856011b77
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-27 18:56:02 +0100

    create search aggregate and implement

--- src/lib/mongo/actions/articles/query/index.ts
+++ src/lib/mongo/actions/articles/query/index.ts
@@ -1,10 +1,7 @@
 import Article from "@/models/Article";
 import { GetLatestArticlesProps } from "../search";
-import { match, matchQuery } from "./match";
-import { destructProvider } from "./populate";
 import { PipelineStage } from "mongoose";
-import { sortCollection } from "./sort";
-import { setLimit } from "./limit";
+import { createSearchAggregate } from "./searchAggregate";
 
 // Aggregation course 2 hour
 // https://www.youtube.com/watch?v=vx1C8EyTa7Y
@@ -13,22 +10,14 @@ export type Aggregator = PipelineStage[];
 
 const createAggregate = (queryParams: GetLatestArticlesProps) => {
 	const aggregate: Aggregator = [];
-	// queried
-	const queryAdded = matchQuery(queryParams, aggregate);
-	// populated
-	const providerAdded = destructProvider(queryAdded);
-	// call match all
-	const matched = match(queryParams, providerAdded);
-	// call sort
-	const sorted = sortCollection(queryParams, matched);
-	// @ts-expect-error - typing error my sort is not correct type
-	const limited = setLimit(queryParams, sorted);
-	return limited;
+
+	return createSearchAggregate(queryParams, aggregate);
 };
 
 export const buildArticleSearchQuery = async (
 	queryParams: GetLatestArticlesProps
 ) => {
 	const res = await Article.aggregate(createAggregate(queryParams));
+
 	return res;
 };
