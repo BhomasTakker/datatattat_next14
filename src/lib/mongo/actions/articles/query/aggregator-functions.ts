35f3b4f74d0d3e4f94e124cffd3d80a547b574d3
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-05-05 00:43:59 +0100

    add and apply trust match
    refactor search aggregate

---
+++ src/lib/mongo/actions/articles/query/aggregator-functions.ts
@@ -0,0 +1,82 @@
+import ArticleProvider from "@/models/ArticleProvider";
+import { Aggregator } from ".";
+
+export const addProviderLookup = (aggregator: Aggregator) => {
+	aggregator.push({
+		$lookup: {
+			from: ArticleProvider.collection.name,
+			localField: "provider",
+			foreignField: "_id",
+			as: "provider",
+		},
+	});
+};
+
+export const addFields = (aggregator: Aggregator) => {
+	aggregator.push({
+		$addFields: {
+			score: { $meta: "searchScore" },
+			scoreDetails: { $meta: "searchScoreDetails" },
+			provider: { $arrayElemAt: ["$provider", 0] },
+		},
+	});
+};
+
+export const matchTrust = (
+	aggregator: Aggregator,
+	trustHigher: string | undefined,
+	trustLower: string | undefined
+) => {
+	// must be numbers etc
+	if (trustHigher || trustLower) {
+		const higher = trustHigher ? +trustHigher : 0;
+		const lower = trustLower ? +trustLower : 100;
+
+		aggregator.push({
+			$match: {
+				$expr: {
+					$and: [
+						{ $gte: ["$provider.rating", higher] },
+						{ $lte: ["$provider.rating", lower] },
+					],
+				},
+			},
+		});
+	}
+};
+
+export const matchLeaning = (
+	aggregator: Aggregator,
+	leaningHigher: string | undefined,
+	leaningLower: string | undefined
+) => {
+	if (leaningHigher || leaningLower) {
+		const higher = leaningHigher ? +leaningHigher : -1;
+		const lower = leaningLower ? +leaningLower : 1;
+		aggregator.push({
+			$match: {
+				$expr: {
+					$and: [
+						{ $gte: ["$provider.leaning", higher] },
+						{ $lte: ["$provider.leaning", lower] },
+					],
+				},
+			},
+		});
+	}
+};
+
+export const matchOrigin = (
+	aggregator: Aggregator,
+	origin: string | undefined
+) => {
+	if (origin) {
+		aggregator.push({
+			$match: {
+				$expr: {
+					$and: [{ $eq: ["$provider.origin", origin] }],
+				},
+			},
+		});
+	}
+};
--- src/lib/mongo/actions/articles/query/searchAggregate.ts
+++ src/lib/mongo/actions/articles/query/searchAggregate.ts
@@ -1,122 +1,40 @@
-import ArticleProvider from "@/models/ArticleProvider";
 import { Aggregator } from ".";
 import { GetLatestArticlesProps } from "../search";
+import {
+	addFilter,
+	addDateRange,
+	addDurationRange,
+	addSort,
+	addMinimumShouldMatch,
+	getLimit,
+} from "./search-query-functions";
+import {
+	addFields,
+	addProviderLookup,
+	matchLeaning,
+	matchOrigin,
+	matchTrust,
+} from "./aggregator-functions";
 
 // MongoDB Atlas Search Course
 // https://learn.mongodb.com/learning-paths/atlas-search
 
 // Clean me up!!
 
-const HARD_LIMIT = 100;
-
-const getLimit = (queryParams: GetLatestArticlesProps) => {
-	const { limit = HARD_LIMIT } = queryParams;
-	let limitToUse = +limit <= HARD_LIMIT ? +limit : HARD_LIMIT;
-
-	if (isNaN(+limit) || !limit) {
-		limitToUse = HARD_LIMIT;
-	}
-	return limitToUse;
-};
-
-const addFilter = (
-	filter: any[],
-	query: string | number | boolean | object | unknown[],
-	path: string,
-	type: string = "text"
-) => {
-	if (query) {
-		filter.push({
-			[type]: {
-				query,
-				path,
-			},
-		});
-	}
-	return filter;
-};
-
-const addDurationRange = (
-	filter: any[],
-	queryParams: GetLatestArticlesProps
-) => {
-	const { durationHigher, durationLower } = queryParams;
-	if (durationHigher || durationLower) {
-		filter.push({
-			range: {
-				path: "media.duration",
-				gt: durationHigher ? +durationHigher : undefined,
-				lt: durationLower ? +durationLower : undefined,
-			},
-		});
-	}
-};
-
-const addDateRange = (filter: any[], queryParams: GetLatestArticlesProps) => {
-	const { before, after } = queryParams;
-	if (before || after) {
-		filter.push({
-			range: {
-				path: "details.published",
-				gt: after ? new Date(after) : undefined,
-				lt: before ? new Date(before) : undefined,
-			},
-		});
-	}
-	return filter;
-};
-
-// A little ugly / do better
-const addMinimumShouldMatch = (queryParams: GetLatestArticlesProps) => {
-	const { minimumShouldMatch, shouldContain = [] } = queryParams;
-
-	if (minimumShouldMatch) {
-		const isValid = +minimumShouldMatch >= 0 && +minimumShouldMatch <= 100;
-		if (isValid && shouldContain.length >= +minimumShouldMatch) {
-			return +minimumShouldMatch;
-		}
-		return 0;
-	}
-	return 0;
-};
-
-const AvailableSort = {
-	NONE: "none",
-	RELEVANCE: "relevance",
-	DATE_ASCENDING: "date-ascending",
-	DATE_DESCENDING: "date-descending",
-} as const;
-
-export const addSort = (queryParams: GetLatestArticlesProps) => {
-	const { sort } = queryParams;
-
-	switch (sort) {
-		case AvailableSort.RELEVANCE:
-			// check a query!
-			return {
-				score: {
-					$meta: "searchScore",
-					order: 1,
-				},
-			};
-		case AvailableSort.DATE_ASCENDING:
-			return { "details.published": 1 };
-		case AvailableSort.DATE_DESCENDING:
-			return { "details.published": -1 };
-		default:
-			return undefined;
-	}
-};
-
 export const createSearchAggregate = (
 	queryParams: GetLatestArticlesProps,
-	aggregaor: Aggregator
+	aggregator: Aggregator
 ) => {
 	const {
 		query,
 		variant,
 		region,
 		language,
+		trustHigher,
+		trustLower,
+		leaningHigher,
+		leaningLower,
+		origin,
 		mustContain = [],
 		mustNotContain = [],
 		shouldContain = [],
@@ -167,7 +85,7 @@ export const createSearchAggregate = (
 	const isShould = should.length > 0 ? { should } : {};
 	const isMustNot = mustNot.length > 0 ? { mustNot } : {};
 
-	aggregaor.push({
+	aggregator.push({
 		$search: {
 			// Also working on our date index
 			index: "title",
@@ -189,23 +107,16 @@ export const createSearchAggregate = (
 		},
 	});
 	// we need to use provider for filtering trust
-	aggregaor.push({
-		$lookup: {
-			from: ArticleProvider.collection.name,
-			localField: "provider",
-			foreignField: "_id",
-			as: "provider",
-		},
-	});
-	aggregaor.push({
-		$addFields: {
-			score: { $meta: "searchScore" },
-			scoreDetails: { $meta: "searchScoreDetails" },
-			provider: { $arrayElemAt: ["$provider", 0] },
-		},
-	});
+	addProviderLookup(aggregator);
+	addFields(aggregator);
+	// Technically I don't think we should do this
+	// match after search is known for slowdowns
+	// but I don't know another way to do this
+	matchTrust(aggregator, trustHigher, trustLower);
+	// Ready and seem fine but removed for now
+	// matchLeaning(aggregator, leaningHigher, leaningLower);
+	// matchOrigin(aggregator, origin);
+	aggregator.push({ $limit: getLimit(queryParams) });
 
-	aggregaor.push({ $limit: getLimit(queryParams) });
-
-	return aggregaor;
+	return aggregator;
 };
