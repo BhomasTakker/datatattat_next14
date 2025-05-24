232dd50c5354405c35219b120a783c52f132a40b
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-05-24 19:17:33 +0100

    minor

--- src/app/users/[username]/profile/page.test.tsx
+++ src/app/users/[username]/profile/page.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import UserProfile from "./page";
 import { redirect } from "next/navigation";
 import { PATHS } from "@/lib/routing/paths";
