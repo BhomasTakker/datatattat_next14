ba7904fa5883d393643805521926d64d292d6c91
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-06-02 01:17:22 +0100

    update job and rename github env vars

--- src/lib/next-auth/providers/github.ts
+++ src/lib/next-auth/providers/github.ts
@@ -2,10 +2,10 @@ import Github from "next-auth/providers/github";
 import { loginOrSignUp } from "../user-login";
 import { returnUser } from "../user-return";
 
-const { GITHUB_ID, GITHUB_SECRET } = process.env;
+const { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET } = process.env;
 
 // don't error just don't return login with github
-if (!GITHUB_ID || !GITHUB_SECRET) {
+if (!AUTH_GITHUB_ID || !AUTH_GITHUB_SECRET) {
 	throw new Error("Github ID and Secret not found");
 }
 
@@ -14,6 +14,6 @@ export const GITHUB = Github({
 		const user = await loginOrSignUp(profile, "github");
 		return returnUser(profile, user, "github");
 	},
-	clientId: GITHUB_ID,
-	clientSecret: GITHUB_SECRET,
+	clientId: AUTH_GITHUB_ID,
+	clientSecret: AUTH_GITHUB_SECRET,
 });
