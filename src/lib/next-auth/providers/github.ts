25ed44d79b424dcd5e15f60f2d72e8c338ca72f1
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-06-02 02:00:01 +0100

    tests

--- src/lib/next-auth/providers/github.ts
+++ src/lib/next-auth/providers/github.ts
@@ -5,15 +5,15 @@ import { returnUser } from "../user-return";
 const { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET } = process.env;
 
 // don't error just don't return login with github
-if (!AUTH_GITHUB_ID || !AUTH_GITHUB_SECRET) {
-	throw new Error("Github ID and Secret not found");
-}
+// if (!AUTH_GITHUB_ID || !AUTH_GITHUB_SECRET) {
+// 	throw new Error("Github ID and Secret not found");
+// }
 
 export const GITHUB = Github({
 	async profile(profile) {
 		const user = await loginOrSignUp(profile, "github");
 		return returnUser(profile, user, "github");
 	},
-	clientId: AUTH_GITHUB_ID,
-	clientSecret: AUTH_GITHUB_SECRET,
+	clientId: AUTH_GITHUB_ID || "",
+	clientSecret: AUTH_GITHUB_SECRET || "",
 });
