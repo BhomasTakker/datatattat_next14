d448260bda84ecae86af1b72ebe056f6025362c7
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-06-03 19:22:10 +0100

    refactor header page object and add/update header tests

--- automation/page-objects.ts/header-page.ts
+++ automation/page-objects.ts/header-page.ts
@@ -2,29 +2,45 @@ import { Locator, Page } from "@playwright/test";
 import { Base } from "./base";
 
 export class HeaderPage extends Base {
+	private readonly header: Locator;
+	private readonly menuButton: Locator;
+	private readonly homeLogoButton: Locator;
+	private readonly donateButton: Locator;
+	private readonly title: Locator;
+
 	constructor(page: Page) {
 		super(page);
+		this.header = this.page.locator("header");
+		this.menuButton = this.header.locator('[class^="user-menu_menuButton"]');
+		this.homeLogoButton = this.header.locator('a[aria-label="Home"]');
+		this.donateButton = this.header.locator(
+			'a[href="https://ko-fi.com/Z8Z81DQMUH"]'
+		);
+		this.title = this.header.getByTestId("datatattat");
 	}
 	// get header html element i.e.
 	getHeader(): Locator {
-		return this.page.locator("header");
+		return this.header;
 	}
 	// we should get the header 'title' button
-	async getTitle(): Promise<string> {
-		const header = this.getHeader();
-		const h2Locator = header.locator("h2");
-		const textContent = await h2Locator.textContent();
-		return textContent ? textContent.trim() : "";
+	getTitle(): Locator {
+		return this.title;
 	}
-	// get header link by route / this isn't how a user would navigate
-	async getRouteLink(route: string): Promise<Locator> {
-		const header = this.getHeader();
 
-		return header.getByRole("link", { name: route });
+	async getRouteLink(route: string): Promise<Locator> {
+		return this.header.getByRole("link", { name: route });
 	}
 	async getUserMenu(): Promise<Locator> {
 		// probably add and use aria label
-		return this.page.locator('[class^="user-menu_menuButton"]');
+		return this.menuButton;
+	}
+
+	async getUserHomeLogoButton(): Promise<Locator> {
+		return this.homeLogoButton;
+	}
+
+	async getDonateButton(): Promise<Locator> {
+		return this.donateButton;
 	}
 
 	async openUserMenu(): Promise<Locator> {
