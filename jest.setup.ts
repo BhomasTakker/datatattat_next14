d4045e1f25bd787bdc955a666e6f9247fa47f422
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-06-19 19:21:19 +0100

    jest unit refactor
    Create organised mock files

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom";
+import "./mock/mongo";
 
 import { TextEncoder, TextDecoder } from "util";
 
@@ -18,100 +19,5 @@ jest.mock("./src/lib/services/intialise-services", () => {
 		initialiseServices: jest.fn().mockResolvedValue(Promise.resolve()),
 	};
 });
-
-jest.mock("./src/lib/mongo/db", () => ({
-	__esModule: true,
-	connectToMongoDB: jest.fn().mockResolvedValue(undefined),
-}));
-
-jest.mock("./src/models/Article", () => ({
-	__esModule: true,
-	default: {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		findOneAndUpdate: jest.fn(),
-		create: jest.fn(),
-		updateOne: jest.fn(),
-		deleteOne: jest.fn(),
-		collection: {
-			name: "Article",
-		},
-	},
-}));
-
-jest.mock("./src/models/ArticleCollection", () => ({
-	__esModule: true,
-	default: {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		findOneAndUpdate: jest.fn(),
-		create: jest.fn(),
-		updateOne: jest.fn(),
-		deleteOne: jest.fn(),
-		collection: {
-			name: "ArticleCollection",
-		},
-	},
-}));
-
-jest.mock("./src/models/ArticleProvider", () => ({
-	__esModule: true,
-	default: {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		findOneAndUpdate: jest.fn(),
-		create: jest.fn(),
-		updateOne: jest.fn(),
-		deleteOne: jest.fn(),
-		collection: {
-			name: "ArticleProvider",
-		},
-	},
-}));
-
-jest.mock("./src/models/Header", () => ({
-	__esModule: true,
-	default: {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		findOneAndUpdate: jest.fn(),
-		create: jest.fn(),
-		updateOne: jest.fn(),
-		deleteOne: jest.fn(),
-		collection: {
-			name: "Header",
-		},
-	},
-}));
-
-jest.mock("./src/models/Page", () => ({
-	__esModule: true,
-	default: {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		findOneAndUpdate: jest.fn(),
-		create: jest.fn(),
-		updateOne: jest.fn(),
-		deleteOne: jest.fn(),
-		collection: {
-			name: "Page",
-		},
-	},
-}));
-
-jest.mock("./src/models/User", () => ({
-	__esModule: true,
-	default: {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		findOneAndUpdate: jest.fn(),
-		create: jest.fn(),
-		updateOne: jest.fn(),
-		deleteOne: jest.fn(),
-		collection: {
-			name: "User",
-		},
-	},
-}));
 
 Object.assign(global, { TextDecoder, TextEncoder });
