174fdb76c3b384fc9823ccca67edf75f00da6020
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-06-25 19:36:25 +0100

    minor note

--- src/lib/api/component-data/oembed/options/twitter.ts
+++ src/lib/api/component-data/oembed/options/twitter.ts
@@ -11,6 +11,7 @@ const createQueryParameters = () => {
 export const twitterOembed = {
 	// config
 	script: "https://platform.twitter.com/widgets.js",
+	// we need to pass width etc to the oembed endpoint
 	createUrl: (params: TwitterOEmbedParams) => {
 		const { account, tweetId } = params;
 		// timeline is not currently working so we will just return a tweet
