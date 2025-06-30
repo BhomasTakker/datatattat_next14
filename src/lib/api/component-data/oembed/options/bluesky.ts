555761ed839d74ccfb4dd7407b4743b3a73b392d
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-06-30 16:58:36 +0100

    rough imp

--- src/lib/api/component-data/oembed/options/bluesky.ts
+++ src/lib/api/component-data/oembed/options/bluesky.ts
@@ -2,14 +2,23 @@ import { BlueskyOEmbedParams } from "@/types/data-structures/oembed";
 import { createQueryParameters } from "../oembed-query-params";
 
 const baseUrl = "https://embed.bsky.app/oembed";
+const scriptUrl = "https://embed.bsky.app/static/embed.js";
 
 export const blueskyOembed = {
-	script: "https://embed.bsky.app/static/embed.js",
+	script: scriptUrl,
 	createUrl: (params: BlueskyOEmbedParams) => {
 		const { account, postId } = params;
 		const queryParams = createQueryParameters();
 
 		const url = `https://bsky.app/profile/${account}/post/${postId}`;
 		return `${baseUrl}?url=${encodeURIComponent(url)}&${queryParams}`;
+	},
+};
+
+export const blueskyOembedByUri = {
+	script: scriptUrl,
+	createUrl: (uri: string) => {
+		const queryParams = createQueryParameters();
+		return `${baseUrl}?url=${encodeURIComponent(uri)}&${queryParams}`;
 	},
 };
--- src/lib/bluesky/query/index.ts
+++ src/lib/bluesky/query/index.ts
@@ -1,8 +1,39 @@
+import { AppBskyFeedPost, AtpAgent } from "@atproto/api";
+
 export const blueSkyFetch = async (params: any) => {
-	console.log("BlueSky fetch called with params:", params);
+	const blueSkyAgent = new AtpAgent({
+		service: "https://public.api.bsky.app",
+	});
+	// const feeds = await blueSkyAgent.app.bsky.unspecced.getPopularFeedGenerators({
+	// 	limit: 10,
+	// });
+	const feed = await blueSkyAgent.app.bsky.feed.getFeed({
+		feed: "at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot",
+		limit: 10,
+	});
+
+	feed.data.feed.forEach((item: any) => {
+		const { likeCount, replyCount, record, embed } = item.post;
+
+		if (record.embed.video) {
+			console.log("Video embed found:", record.embed.video);
+		}
+	});
+
+	const uris = feed.data.feed.map((item: any) => {
+		const post = item.post as AppBskyFeedPost.Record;
+		if (!AppBskyFeedPost.isRecord(post.record)) return null;
+		return post.uri;
+	});
+
+	// I think what we can expect is a list of posts in most circumstances
+	// We can then use those posts to effectively cretae a feed of
+	// oembd components IF bluesky oembed does not have a feed endpoint
 	return {
 		error: null,
 		test: "HELLO BLUESKY!",
-		items: [],
+		// This check isn't working it seems
+		// fix me and clean
+		items: uris.filter((uri: string | null) => uri !== null),
 	};
 };
