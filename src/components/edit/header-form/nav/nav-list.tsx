8a6eef8e44c9d68fa0c978ce59fb0e4d8cc5b27a
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-03 17:57:39 +0100

    'save' the header form when interact to avoid corruption

--- src/components/edit/header-form/nav/nav-list.tsx
+++ src/components/edit/header-form/nav/nav-list.tsx
@@ -19,7 +19,7 @@ export const NavList = ({
 	pages: IPage[];
 }) => {
 	const [navLinks, setNavLinks] = useState<HeaderNav>([]);
-	const { unregister, setValue, getValues } = useFormContext();
+	const { unregister, setValue, getValues, handleSubmit } = useFormContext();
 	const searchParams = useSearchParams();
 
 	const route = searchParams.get("route") || "/";
@@ -31,10 +31,14 @@ export const NavList = ({
 	}, [links]);
 
 	// potentially create a context provider
-	const onMove = (index: number, direction: string) => {
+	const onMove = async (index: number, direction: string) => {
 		// protect against moving out of bounds
 		if (index === 0 && direction === "up") return;
 		if (index === navLinks.length - 1 && direction === "down") return;
+
+		// We need to trigger validation to 'sav' values
+		// and protect against corruption of values
+		await handleSubmit(() => {})(); // trigger validation
 
 		const newIndex = direction === "up" ? index - 1 : index + 1;
 		// const toSwap = navLinks[newIndex];
@@ -65,8 +69,12 @@ export const NavList = ({
 			return newLinks;
 		});
 	};
-	const onDelete = (index: number) => {
+	const onDelete = async (index: number) => {
 		const newItems = [...navLinks];
+
+		// We need to trigger validation to 'sav' values
+		// and protect against corruption of values
+		await handleSubmit(() => {})(); // trigger validation
 
 		for (let i = index; i < navLinks.length; i++) {
 			const isLastItem = i === navLinks.length - 1;
@@ -89,7 +97,10 @@ export const NavList = ({
 		setNavLinks(newItems.slice(0, newItems.length - 1));
 	};
 
-	const onAdd = () => {
+	const onAdd = async () => {
+		// We need to trigger validation to 'sav' values
+		// and protect against corruption of values
+		await handleSubmit(() => {})(); // trigger validation
 		// Add a new link
 		// create some starting text - route should be i.e. /new-test/
 		const newLink = { label: "", route: route };
