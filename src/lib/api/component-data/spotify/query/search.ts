53361230d183a6b8c6742522ecadea74fe5a9766
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-25 13:57:43 +0100

    minor refactor

--- src/lib/api/component-data/spotify/query/search.ts
+++ src/lib/api/component-data/spotify/query/search.ts
@@ -1,20 +1,31 @@
 import { SearchParams, SearchResponse } from "@/types/api/spotify";
 import { getSpotifyAccessToken } from "./get-access-token";
 
-// we need to provide a sort
-// potentially filter
-// the spotify api is pretty lame for filtering etc
+// config
+const SPOTIFY_SEARCH_URL = "https://api.spotify.com/v1/search";
+
+// you could make this a util
+// if in given object add
+const createUrl = ({
+	q,
+	type,
+	market,
+	limit,
+	offset,
+	include_external,
+}: SearchParams) => {
+	const fetchUrl = new URL(SPOTIFY_SEARCH_URL);
+	fetchUrl.searchParams.append("q", q);
+	fetchUrl.searchParams.append("type", type);
+	if (market) fetchUrl.searchParams.append("market", market);
+	if (limit) fetchUrl.searchParams.append("limit", limit.toString());
+	if (offset) fetchUrl.searchParams.append("offset", offset.toString());
+	if (include_external)
+		fetchUrl.searchParams.append("include_external", include_external);
+	return fetchUrl;
+};
 
 export const search = async (params: SearchParams) => {
-	// console.log("Searching spotify with params:", params);
-	const {
-		q,
-		type,
-		market,
-		limit: searchLimit,
-		offset: searchOffset,
-		include_external,
-	} = params;
 	const accessToken = await getSpotifyAccessToken();
 
 	if (!accessToken) {
@@ -22,14 +33,13 @@ export const search = async (params: SearchParams) => {
 		return [];
 	}
 
-	const response = await fetch(
-		`https://api.spotify.com/v1/search?q=${q}&type=${type}`,
-		{
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		}
-	);
+	const fetchUrl = createUrl(params);
+
+	const response = await fetch(fetchUrl.toString(), {
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
 
 	if (!response.ok) {
 		console.error("Failed to search spotify");
