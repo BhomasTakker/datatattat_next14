4a9065a7a4a2db15cdbc21e98aac1f5bd27f2c8b
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-30 12:02:52 +0100

    silly miss

--- src/components/content/components/spotify-collection/variant-map.ts
+++ src/components/content/components/spotify-collection/variant-map.ts
@@ -2,7 +2,7 @@ import spotifyAudioStack from "./audio-stack/audio-stack";
 import spotifyOembed from "./oembed/oembed";
 
 // type is only different in the renderMethod - JSX.Element & JSX.Element[]
-type Variants = typeof spotifyOembed;
+type Variants = typeof spotifyOembed | typeof spotifyAudioStack;
 
 export enum SpotifyCollectionVariants {
 	SpotifyOembed = "spotify-oembed",
