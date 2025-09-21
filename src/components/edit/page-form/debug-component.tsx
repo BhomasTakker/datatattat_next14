95529d9d193c8565ed8607beddffdc1c84c1a110
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-09-21 14:47:39 +0100

    update debug

--- src/components/edit/page-form/debug-component.tsx
+++ src/components/edit/page-form/debug-component.tsx
@@ -2,18 +2,16 @@
 
 import { Button } from "@/components/ui/button";
 import { useContext } from "react";
-import { useForm, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { EditContext } from "../context/edit-context";
 
 export const DebugComponent = () => {
-	console.log("HOOK:FORM RENDER: DebugComponent");
 	const { handleSubmit } = useFormContext();
 	const { submitDebugHandler } = useContext(EditContext);
 
 	return (
 		<Button
 			onClick={() => {
-				console.log("HOOK:FORM How so??");
 				if (submitDebugHandler) {
 					handleSubmit(submitDebugHandler)();
 				}
