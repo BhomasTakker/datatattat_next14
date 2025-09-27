a55fd2d7cf8e6a33b003b78ef039f251fb8085f3
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-09-27 20:37:16 +0100

    tests for check user auth

--- src/actions/auth/check-user-auth.ts
+++ src/actions/auth/check-user-auth.ts
@@ -7,7 +7,7 @@ import { initialiseServices } from "@/lib/services/intialise-services";
 import { Session } from "@/types/auth/session";
 import { getServerSession } from "next-auth";
 
-const checkUserAuthorized = async (username: string, route: string) => {
+export const checkUserAuthorized = async (username: string, route: string) => {
 	const userHome = PATHS.user(username);
 
 	if (route === userHome) {
@@ -22,7 +22,7 @@ const checkUserAuthorized = async (username: string, route: string) => {
 	}
 };
 
-const checkAdminAuthorized = async (role: string) => {
+export const checkAdminAuthorized = async (role: string) => {
 	if (role !== "admin") {
 		throw new Error("Unauthorized");
 	}
