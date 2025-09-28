0582461952a2639ca5ae89b06ccc7888b1c07e79
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-09-28 10:19:57 +0100

    modal container refactor

--- src/components/edit/page-form/page-form-container.tsx
+++ src/components/edit/page-form/page-form-container.tsx
@@ -13,57 +13,8 @@ import { savePage } from "@/actions/edit/update-page";
 import { useRouter } from "next/navigation";
 import { EditContextProvider } from "../context/edit-context";
 import { randomKeyGenerator } from "@/utils/edit";
-import {
-	createToastAction,
-	initToastPromise,
-	ToastType,
-} from "@/lib/sonner/toast";
-import { FormModal } from "../form-modal/modal";
-import { SaveTemplateForm } from "../template/save-template-form";
-import { LoadTemplateForm } from "../template/load-template-form";
-import {
-	checkTemplateNameUnique,
-	loadTemplate,
-	saveTemplate,
-} from "@/actions/edit/template";
-
-const getFormData = (el: HTMLFormElement) => {
-	const formData = new FormData(el);
-	return Object.fromEntries(formData.entries());
-};
-
-const getTemplateId = (el: HTMLFormElement) => {
-	const data = getFormData(el);
-
-	const { templateId } = data;
-	if (!templateId) {
-		console.error("No template ID provided");
-		return null;
-	}
-	// regex test a-z - only allow letters, numbers, underscores, and hyphens
-
-	if (typeof templateId !== "string") {
-		console.error("Invalid template ID");
-		return null;
-	}
-
-	return templateId;
-};
-
-const checkFormIdValid = (templateId: string | null) => {
-	if (!templateId) return false;
-
-	// regex test a-z - only allow letters, numbers, underscores, and hyphens
-	const validId = /^[a-zA-Z0-9_-]+$/.test(templateId);
-	if (!validId) {
-		console.error(
-			"Invalid template ID. Only letters, numbers, underscores, and hyphens are allowed."
-		);
-		return false;
-	}
-
-	return true;
-};
+import { initToastPromise, ToastType } from "@/lib/sonner/toast";
+import { ModalContainer } from "./modal-container";
 
 export const PageFormContainer = ({ pageData }: { pageData: IPage }) => {
 	const [templateData, setTemplateData] = useState({} as IPage);
@@ -144,68 +95,6 @@ export const PageFormInteractionController = ({
 		setShowLoadTemplateModal(true);
 	};
 
-	const loadTemplateFormSubmitHandler = async (
-		e: React.FormEvent<HTMLFormElement>
-	) => {
-		e.preventDefault();
-
-		const templateId = getTemplateId(e.target as HTMLFormElement);
-		if (!templateId) return;
-
-		initToastPromise({
-			cb: () => loadTemplate(templateId),
-			id: ToastType.LoadTemplate,
-			onComplete: (template) => setTemplate(template),
-		});
-	};
-
-	const saveTemplateFormSubmitHandler = async (
-		e: React.FormEvent<HTMLFormElement>
-	) => {
-		e.preventDefault();
-		const formData = getFormData(e.target as HTMLFormElement);
-		const templateId = getTemplateId(e.target as HTMLFormElement);
-		if (!templateId) return;
-
-		if (!checkFormIdValid(templateId)) return;
-
-		// how to get the current form data here?
-		const currentData = methods.getValues();
-		const update = { ...pageData, ...currentData };
-
-		// check if unique id
-		const isUniqueId = await checkTemplateNameUnique(templateId);
-		if (!isUniqueId) {
-			createToastAction({
-				cb: async () => {
-					setShowSaveTemplateModal(false);
-					await saveTemplate(templateId, update);
-				},
-				id: ToastType.ConfirmSaveTemplate,
-				onComplete: (_res) => {
-					methods.reset(formData);
-				},
-				onError: (err) => {
-					console.error(err);
-				},
-			});
-			return;
-		}
-
-		setShowSaveTemplateModal(false);
-		initToastPromise({
-			// if none use dummy timeout function
-			cb: () => saveTemplate(templateId, update),
-			id: ToastType.SaveTemplate,
-			onComplete: (_res) => {
-				methods.reset(formData);
-			},
-			onError: (err) => {
-				console.error(err);
-			},
-		});
-	};
-
 	return (
 		<EditContextProvider
 			value={{
@@ -215,18 +104,15 @@ export const PageFormInteractionController = ({
 				submitDebugHandler: debugHandler,
 			}}
 		>
-			<FormModal
-				isOpen={showSaveTemplateModal}
-				onClose={() => setShowSaveTemplateModal(false)}
-			>
-				<SaveTemplateForm submitHandler={saveTemplateFormSubmitHandler} />
-			</FormModal>
-			<FormModal
-				isOpen={showLoadTemplateModal}
-				onClose={() => setShowLoadTemplateModal(false)}
-			>
-				<LoadTemplateForm submitHandler={loadTemplateFormSubmitHandler} />
-			</FormModal>
+			<ModalContainer
+				methods={methods}
+				pageData={pageData}
+				showSaveTemplateModal={showSaveTemplateModal}
+				setShowSaveTemplateModal={setShowSaveTemplateModal}
+				showLoadTemplateModal={showLoadTemplateModal}
+				setShowLoadTemplateModal={setShowLoadTemplateModal}
+				setTemplate={setTemplate}
+			/>
 			<FormProvider {...methods}>
 				<PageForm
 					submitHandler={submitHandler}
