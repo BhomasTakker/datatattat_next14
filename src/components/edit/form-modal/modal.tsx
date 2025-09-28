d739fbc0ef02375be54c6da279d9e73c4d5b22f7
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-09-28 11:40:39 +0100

    bg click to close and propogation

--- src/components/edit/form-modal/modal.tsx
+++ src/components/edit/form-modal/modal.tsx
@@ -8,11 +8,14 @@ type FormModalProps = {
 	children: React.ReactNode;
 };
 
+const blockHandler = (e: React.MouseEvent) => e.stopPropagation();
+
 export const FormModal = ({ isOpen, onClose, children }: FormModalProps) => {
 	if (!isOpen) return null;
+
 	return (
-		<div className={styles.root}>
-			<div className={styles.content}>
+		<div className={styles.root} onClick={onClose}>
+			<div className={styles.content} onClick={blockHandler}>
 				<div className={styles.closeButton}>
 					<IconButton data-testid="close" icon={IoMdClose} onClick={onClose} />
 				</div>
