1937b20044259b2ca8d68f2daec71518faff0ff6
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-11 14:51:51 +0000

    Add delete functionality and improve article form layout in CMS

--- src/components/cms/forms/article/article-form.tsx
+++ src/components/cms/forms/article/article-form.tsx
@@ -10,6 +10,9 @@ import { CollectionItem } from "@/types/data-structures/collection/item/item";
 
 import { Article } from "@/components/content/components/article-collection/article/article";
 import articleStyles from "@/components/content/components/article-collection/article/article-display.module.scss";
+import { createToastAction, initToastPromise } from "@/lib/sonner/toast";
+import { CMSToastType } from "../../toast";
+import { deleteArticle, updateArticle } from "@/actions/cms/article";
 
 type ArticleCMSFormProps = {
 	article: CollectionItem;
@@ -23,8 +26,26 @@ export const ArticleCMSForm = ({ article }: ArticleCMSFormProps) => {
 	});
 
 	const submitHandler = methods.handleSubmit(async (data) => {
-		console.log(`Submitting data:`, data);
+		initToastPromise({
+			cb: () =>
+				updateArticle({
+					...data,
+				}),
+			id: CMSToastType.SaveArticle,
+			onComplete: (article) => console.log("Article saved:", article),
+		});
 	});
+
+	// I don't think this is really an option for Articles
+	const deleteArticleHandler = async () => {
+		console.log(`Deleting article with id: ${article._id}`);
+		createToastAction({
+			cb: () => deleteArticle(article._id as string),
+			id: CMSToastType.DeleteArticle,
+			onComplete: (_res) => Promise.resolve(console.log("Article Deleted.")),
+			onError: (err) => console.error(err), // launch error toast
+		});
+	};
 
 	return (
 		<FormProvider {...methods}>
@@ -32,7 +53,10 @@ export const ArticleCMSForm = ({ article }: ArticleCMSFormProps) => {
 			<form onSubmit={submitHandler} className={styles.form}>
 				<h2>Fetch Articles CMS Form</h2>
 				<InputFactory data={{ ...ARTICLE_CONFIG }} />
-				<Button type="submit">Submit</Button>
+				<div className={styles.buttons}>
+					<Button type="submit">Submit</Button>
+					<Button onClick={deleteArticleHandler}>Delete</Button>
+				</div>
 			</form>
 		</FormProvider>
 	);
