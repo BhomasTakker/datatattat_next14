fe009ec84e234696544629cc5526d2870c2b9742
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-11 14:50:04 +0000

    Clarify comment in utils.ts to emphasize that getCMSHeaders should be a server-only function

--- src/components/cms/utils.ts
+++ src/components/cms/utils.ts
@@ -2,6 +2,7 @@ const CMS_API_KEY = process.env.CMS_API_KEY || "";
 const CMS_ROUTE = process.env.CMS_ROUTE || "";
 
 export const getRoute = (url: string = "") => CMS_ROUTE + url;
+// This 100% should be a server ONLY function
 export const getCMSHeaders = () => ({
 	"Content-Type": "application/json",
 	"x-api-key": CMS_API_KEY,
