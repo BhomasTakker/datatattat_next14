33e4fd8a92f8f51240b4b12dfbce4ff3a8afd312
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-14 14:32:16 +0000

    Refactor article page to remove unused imports and streamline initialization

--- src/app/(cms)/cms/articles/[id]/page.tsx
+++ src/app/(cms)/cms/articles/[id]/page.tsx
@@ -1,13 +1,10 @@
-import isSignupComplete from "@/actions/signup/signup-completed";
-import isValidSession from "@/actions/auth/check-session";
-import { isAdminUser } from "@/actions/auth/check-valid-user";
-import { connectToMongoDB } from "@/lib/mongo/db";
 import styles from "../page.module.scss";
 import { getArticle } from "@/actions/cms/article";
 import { redirect } from "next/navigation";
 import { ArticleCMSForm } from "@/components/cms/forms/article/article-form";
 import { ArticleProviderCMSForm } from "@/components/cms/forms/article-provider/article-provider-form";
 import { CMSTitle } from "@/components/cms/title/cms-title";
+import { initCMSPage } from "@/actions/cms/init-cms-page";
 
 type Params = Promise<{ id: string }>;
 type Props = {
@@ -15,11 +12,7 @@ type Props = {
 };
 
 export default async function Page({ params }: Props) {
-	// this all seems like it should be in middleware not a page?
-	await connectToMongoDB();
-	await isValidSession();
-	await isSignupComplete();
-	await isAdminUser();
+	await initCMSPage();
 
 	const { id } = await params;
 
