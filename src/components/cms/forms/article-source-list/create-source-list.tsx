fb5b5e598e4cbcf1dde7a60cbdf2707b65ce107c
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-15 20:32:35 +0000

    Add CreateSourceListForm component for creating new source lists

---
+++ src/components/cms/forms/article-source-list/create-source-list.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { InputFactory } from "@/components/edit/inputs/input-factory";
+import { Button } from "@/components/ui/button";
+import { FormProvider, useForm } from "react-hook-form";
+import styles from "../form.module.scss";
+import { initToastPromise } from "@/lib/sonner/toast";
+import { CMSToastType } from "../../toast";
+import { FormTitle } from "../title/title";
+import { ArticleSourceList } from "@/types/cms/ArticleSourceList";
+import { ARTICLE_SOURCE_LIST_CONFIG } from "../../config/article-source-list.config";
+import { createSourceList } from "@/actions/cms/source-list";
+
+export const CreateSourceListForm = () => {
+	const methods = useForm();
+
+	const submitHandler = methods.handleSubmit(async (data) => {
+		initToastPromise({
+			cb: () => createSourceList(data as ArticleSourceList),
+			id: CMSToastType.CreateArticleSourceList,
+			onComplete: () =>
+				Promise.resolve(console.log("Article Source List Created.")),
+		});
+	});
+
+	return (
+		<FormProvider {...methods}>
+			<form onSubmit={submitHandler} className={styles.form}>
+				<FormTitle
+					title="Create New Source List"
+					subtitle="Add a new source list."
+				/>
+				<InputFactory data={ARTICLE_SOURCE_LIST_CONFIG} />
+				<Button type="submit">Submit</Button>
+			</form>
+		</FormProvider>
+	);
+};
