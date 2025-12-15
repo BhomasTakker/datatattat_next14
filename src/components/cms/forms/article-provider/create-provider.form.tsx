ed20945fb4f9e37ff68c267e996363a56f1bb351
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-15 19:15:54 +0000

    Add CreateProviderForm component for article provider creation

---
+++ src/components/cms/forms/article-provider/create-provider.form.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { InputFactory } from "@/components/edit/inputs/input-factory";
+import { Button } from "@/components/ui/button";
+import { FormProvider, useForm } from "react-hook-form";
+import styles from "../form.module.scss";
+import { useRouter } from "next/navigation";
+import { createProvider } from "@/actions/cms/provider";
+import { createToastAction } from "@/lib/sonner/toast";
+import { CMSToastType } from "../../toast";
+import { ProviderItem } from "@/types/data-structures/collection/item/item";
+import { ARTICLE_PROVIDER_CONFIG } from "../../config/article-povider.config";
+import { FormTitle } from "../title/title";
+
+export const CreateProviderForm = () => {
+	const methods = useForm();
+	const router = useRouter();
+
+	const submitHandler = methods.handleSubmit(async (data) => {
+		createToastAction({
+			cb: () => createProvider(data as ProviderItem),
+			id: CMSToastType.CreateArticleProvider,
+			onComplete: (_res) =>
+				Promise.resolve(console.log("Article Provider Created.")),
+			onError: (err) => console.error(err),
+		});
+	});
+
+	return (
+		<FormProvider {...methods}>
+			<form onSubmit={submitHandler} className={styles.form}>
+				<FormTitle
+					title="Create New Article Provider"
+					subtitle="Add a new provider."
+				/>
+				<InputFactory data={ARTICLE_PROVIDER_CONFIG} />
+				<Button type="submit">Submit</Button>
+			</form>
+		</FormProvider>
+	);
+};
