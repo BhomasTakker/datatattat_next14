95471be8f88cb6b67954a7ed7dec13fd54daf9da
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-15 20:07:35 +0000

    Add CreateSourceForm component for article source creation

---
+++ src/components/cms/forms/article-source/create-article-source.form.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { InputFactory } from "@/components/edit/inputs/input-factory";
+import { Button } from "@/components/ui/button";
+import { FormProvider, useForm } from "react-hook-form";
+import styles from "../form.module.scss";
+import { initToastPromise } from "@/lib/sonner/toast";
+import { CMSToastType } from "../../toast";
+import { FormTitle } from "../title/title";
+import { createSource } from "@/actions/cms/source";
+import { ArticleSource } from "@/types/cms/ArticleSource";
+import { ARTICLE_SOURCE_CONFIG } from "../../config/article-source.config";
+
+export const CreateSourceForm = () => {
+	const methods = useForm();
+
+	const submitHandler = methods.handleSubmit(async (data) => {
+		initToastPromise({
+			cb: () => createSource(data as ArticleSource),
+			id: CMSToastType.CreateArticleSource,
+			onComplete: () => Promise.resolve(console.log("Article Source Created.")),
+		});
+	});
+
+	return (
+		<FormProvider {...methods}>
+			<form onSubmit={submitHandler} className={styles.form}>
+				<FormTitle
+					title="Create New Article Source"
+					subtitle="Add a new source."
+				/>
+				<InputFactory data={ARTICLE_SOURCE_CONFIG} />
+				<Button type="submit">Submit</Button>
+			</form>
+		</FormProvider>
+	);
+};
