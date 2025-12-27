87568f4605543466f1e091c7660bd2cbfef665ca
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-12-27 14:18:02 +0000

    Refactor FetchProvidersCMSForm to streamline provider fetching and enhance search functionality

--- src/components/cms/forms/article-provider/fetch-article-provider-form.tsx
+++ src/components/cms/forms/article-provider/fetch-article-provider-form.tsx
@@ -4,36 +4,58 @@ import { InputFactory } from "@/components/edit/inputs/input-factory";
 import { Button } from "@/components/ui/button";
 import { FormProvider, useForm } from "react-hook-form";
 import styles from "../form.module.scss";
-import { useRouter } from "next/navigation";
-import { getProvider } from "@/actions/cms/provider";
+import { getProviders, gotoProvider } from "@/actions/cms/provider";
 import { FETCH_PROVIDERS_CONFIG } from "../../config/fetch-providers.config";
 import { FormTitle } from "../title/title";
+import { useState } from "react";
+import { CMSTitle } from "../../title/cms-title";
+import { PaginatedTable } from "@/components/content/components/table/paginated-table";
+import { PaginatedData } from "@/types/data-structures/paginated";
 
 export const FetchProvidersCMSForm = () => {
 	const methods = useForm();
-	const router = useRouter();
+	const [paginatedProviders, setPaginatedProviders] =
+		useState<PaginatedData | null>(null);
+	const [searchName, setSearchName] = useState("");
 
 	const submitHandler = methods.handleSubmit(async (data) => {
-		const provider = await getProvider({
-			url: data.url,
+		const providers = await getProviders({
 			name: data.name,
-			id: data.id,
 		});
 
-		// In this instance we 'should' be able to pass the loaded article
-		router.push(`/cms/articles/providers/${provider?._id}`);
+		setPaginatedProviders(providers);
+		setSearchName(data.name);
 	});
 
 	return (
 		<FormProvider {...methods}>
 			<form onSubmit={submitHandler} className={styles.form}>
-				<FormTitle
-					title="Fetch Article Provider"
-					subtitle="Fetch a single existing provider."
-				/>
+				<FormTitle title="Search Providers" subtitle="Search by name." />
 				<InputFactory data={{ ...FETCH_PROVIDERS_CONFIG }} />
 				<Button type="submit">Submit</Button>
 			</form>
+			{paginatedProviders && (
+				<>
+					{/* Different title component */}
+					<CMSTitle title="Search Results" />
+					<PaginatedTable
+						columns={[
+							"name",
+							"description",
+							"url",
+							"rating",
+							"origin",
+							"leaning",
+						]}
+						query={{
+							name: searchName,
+						}}
+						paginatedData={paginatedProviders}
+						fetchPaginatedData={getProviders}
+						onSelect={gotoProvider}
+					/>
+				</>
+			)}
 		</FormProvider>
 	);
 };
