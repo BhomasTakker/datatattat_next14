9bda2a6b62a280c43a2d7baaccca904c990f82c9
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-04-06 20:15:23 +0100

    refactor: move ArticleCollectionVariants enum to a separate file

---
+++ src/components/content/components/article-collection/variants.ts
@@ -0,0 +1,8 @@
+export enum ArticleCollectionVariants {
+	StackScroller = "stack-scroller",
+	StackColumns = "stack-columns",
+	gridDisplay5 = "grid-display-5",
+	gridDisplay7 = "grid-display-7",
+	videoDisplay = "video-display",
+	audioDisplay = "audio-display",
+}
