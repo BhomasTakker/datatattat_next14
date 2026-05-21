0e8f538dddd6657e3d6203f2a86562f03fdd3b53
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-05-21 09:38:39 +0100

    comment

--- src/components/content/components/article-collection/collections/timeline-day/template.tsx
+++ src/components/content/components/article-collection/collections/timeline-day/template.tsx
@@ -1,5 +1,7 @@
 import styles from "./template.module.scss";
 
+// Uses scroller so that template gets used here
+// No actual templating required
 export const TimelineDayTemplate = () => {
 	return <div className={styles.template} />;
 };
