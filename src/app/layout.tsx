057934fcfcc3a3fb797f05a52bf083b6c0950e55
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-05-25 09:07:59 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,18 @@ export const metadata: Metadata = {
 		template: "%s | Datatattat",
 		default: "Datatattat",
 	},
+	icons: {
+		icon: [
+			{ url: "/assets/logo-square.png", type: "image/png", sizes: "32x32" },
+			{ url: "/assets/logo-square.png", type: "image/png", sizes: "192x192" },
+		],
+		shortcut: "/assets/logo-square.png",
+		apple: {
+			url: "/assets/logo-square.png",
+			type: "image/png",
+			sizes: "180x180",
+		},
+	},
 };
 
 export default async function RootLayout({
