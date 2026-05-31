8708f6843a09ac69e4064624a4d8fb3a2a6c5604
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-05-31 17:28:57 +0100

    update configs for pages

--- src/components/edit/config/page/settings-config.ts
+++ src/components/edit/config/page/settings-config.ts
@@ -1,5 +1,16 @@
-import { SwitchInputProps } from "@/types/edit/inputs/inputs";
+import { SelectInputProps, SwitchInputProps } from "@/types/edit/inputs/inputs";
 import { EditInputs } from "../../inputs/inputs";
+
+// If a User page this will be User
+// i.e. we wouldn't allow the option
+export const TYPE_CONFIG: SelectInputProps = {
+	id: "pageType",
+	type: EditInputs.select,
+	label: "Page Type",
+	required: true,
+	defaultValue: "Content",
+	options: ["Content", "User", "Landing"],
+};
 
 export const LIVE_CONFIG: SwitchInputProps = {
 	id: "live",
