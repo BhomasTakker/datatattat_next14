ec7442e9cc2045b3e43d76b291cbc5d9af558e1e
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-06-20 15:09:05 +0100

    Use article collection for search results

--- src/components/page/search/components/article-list.tsx
+++ src/components/page/search/components/article-list.tsx
@@ -1,14 +1,27 @@
 import { CollectionItem } from "@/types/data-structures/collection/item/item";
 import styles from "./article-list.module.scss";
+import { ArticleCollection } from "@/components/content/components/article-collection/article-collection";
+import { ArticleCollectionVariants } from "@/components/content/components/article-collection/variants";
+
+/*
+componentProps={{
+	variant: PlayerCollectionVariant.VerticalScroll,
+	sourceType: PlayerSourceTypes.Youtube,
+}}
+*/
 
 export const ArticleList = ({ articles }: { articles: CollectionItem[] }) => {
 	return (
 		<ul className={styles.articleList}>
-			{articles.map((item) => (
-				<li key={item._id} className={styles.articleItem}>
-					<h2>{item.title}</h2>
-				</li>
-			))}
+			<ArticleCollection
+				component={{
+					componentType: "ArticleCollection",
+					componentProps: {
+						variantType: ArticleCollectionVariants.StackScroller,
+					} as any,
+				}}
+				dataObject={{ data: { items: articles } }}
+			/>
 		</ul>
 	);
 };
