6da37166c6f95142499879aa949e73ed19ef864e
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-06-25 20:18:06 +0100

    update article-list

--- src/components/page/search/components/article-list.tsx
+++ src/components/page/search/components/article-list.tsx
@@ -2,26 +2,38 @@ import { CollectionItem } from "@/types/data-structures/collection/item/item";
 import styles from "./article-list.module.scss";
 import { ArticleCollection } from "@/components/content/components/article-collection/article-collection";
 import { ArticleCollectionVariants } from "@/components/content/components/article-collection/variants";
+import {
+	PlayerCollectionVariant,
+	PlayerSourceTypes,
+} from "@/components/content/components/article-collection/collections/video-display/structs";
 
-/*
-componentProps={{
-	variant: PlayerCollectionVariant.VerticalScroll,
-	sourceType: PlayerSourceTypes.Youtube,
-}}
-*/
+type ArticleListProps = {
+	articles: CollectionItem[];
+	articleVariant?: "article" | "video"; // | 'audio' | 'image' | 'document';
+};
 
-export const ArticleList = ({ articles }: { articles: CollectionItem[] }) => {
+const articleComponentPropsMap = {
+	article: {
+		componentType: "ArticleCollection",
+		componentProps: {
+			variantType: ArticleCollectionVariants.StackScroller,
+		} as any,
+	},
+	video: {
+		componentType: "ArticleCollection",
+		componentProps: {
+			variantType: ArticleCollectionVariants.videoDisplay,
+			sourceType: PlayerSourceTypes.Youtube,
+			variant: PlayerCollectionVariant.VerticalScroll,
+		} as any,
+	},
+} as const;
+
+export const ArticleList = ({ articles, articleVariant }: ArticleListProps) => {
 	return (
-		<ul className={styles.articleList}>
-			<ArticleCollection
-				component={{
-					componentType: "ArticleCollection",
-					componentProps: {
-						variantType: ArticleCollectionVariants.StackScroller,
-					} as any,
-				}}
-				dataObject={{ data: { items: articles } }}
-			/>
-		</ul>
+		<ArticleCollection
+			component={articleComponentPropsMap[articleVariant || "article"]}
+			dataObject={{ data: { items: articles } }}
+		/>
 	);
 };
